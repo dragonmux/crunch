668a19c2914326e0f623655def8ae9dac99a59f8
dx-mon <dx-mon@users.sourceforge.net>
2012-02-20 03:02:00 +0000

    Done all the implementation needed to make libTest usable to some degree for running tests. Next up is searching the directory tree at cwd for viable test libraries and running them

--- libTest/Test.c
+++ libTest/Test.c
@@ -3,8 +3,11 @@
 #include "Core.h"
 #include "Logger.h"
 #include "ArgsParser.h"
+#include "StringFuncs.h"
+#include "Memory.h"
+#include <dlfcn.h>
 
-static const int ok;
+static const int ok = 0;
 const arg args[] =
 {
 	{"--log", 1, 1},
@@ -12,10 +15,15 @@ const arg args[] =
 };
 
 parsedArg **parsedArgs = NULL;
+parsedArg **namedTests = NULL;
+uint32_t numTests = 0;
 
-void *testRunner(void *_)
+void *testRunner(void *self)
 {
-	logResult(RESULT_SUCCESS, "\n");
+	unitTest *test = (unitTest *)self;
+	testPrintf("%s...\n", test->theTest->testName);
+	test->theTest->testFunc();
+	logResult(RESULT_SUCCESS, "");
 	pthreadExit(&ok);
 }
 
@@ -26,15 +34,86 @@ void printStats()
 	if (total == 0)
 		testPrintf("--\n");
 	else
-		testPrintf("%0.2f\n", ((double)passes) / ((double)total));
+		testPrintf("%0.2f\n", ((double)passes) / ((double)total) * 100.0);
+}
+
+uint8_t getTests()
+{
+	uint32_t i, j, n;
+	for (n = 0; parsedArgs[n] != NULL; n++);
+	namedTests = testMalloc(sizeof(parsedArg *) * (n + 1));
+
+	for (j = 0, i = 0; i < n; i++)
+	{
+		if (findArgInArgs(parsedArgs[i]->value) == NULL)
+		{
+			namedTests[j] = parsedArgs[i];
+			j++;
+		}
+	}
+	if (j == 0)
+	{
+		free(namedTests);
+		return FALSE;
+	}
+	else
+	{
+		namedTests = testRealloc(namedTests, sizeof(parsedArg *) * (j + 1));
+		numTests = j;
+		return TRUE;
+	}
+}
+
+uint8_t tryRegistration(void *testSuit)
+{
+	void (__cdecl *registerTests)();
+	registerTests = dlsym(testSuit, "registerTests");
+	if (registerTests == NULL)
+	{
+		dlclose(testSuit);
+		return FALSE;
+	}
+	registerTests();
+	return TRUE;
 }
 
 void runTests()
 {
+	pthread_attr_t threadAttrs;
+	uint32_t i;
+	test *currTest;
 	log *logFile;
+
 	parsedArg *logging = findArg(parsedArgs, "--log", NULL);
 	if (logging != NULL)
 		logFile = startLogging(logging->params[0]);
+	pthread_attr_init(&threadAttrs);
+	pthread_attr_setdetachstate(&threadAttrs, PTHREAD_CREATE_JOINABLE);
+	pthread_attr_setscope(&threadAttrs, PTHREAD_SCOPE_PROCESS);
+
+	for (i = 0; i < numTests; i++)
+	{
+		char *testLib = formatString("%s.so", namedTests[i]->value);
+		void *testSuit = dlopen(testLib, RTLD_LAZY);
+		free(testLib);
+		if (testSuit == NULL || tryRegistration(testSuit) == FALSE)
+		{
+			testPrintf("Test library %s was not a valid library, skipping\n", namedTests[i]->value);
+			continue;
+		}
+		currTest = tests;
+		while (currTest->testFunc != NULL)
+		{
+			unitTest *test = testMalloc(sizeof(unitTest));
+			test->theTest = currTest;
+			test->testThread = testMalloc(sizeof(pthread_t));
+			pthread_create(test->testThread, &threadAttrs, testRunner, test);
+			pthread_join(*test->testThread, NULL);
+			currTest++;
+		}
+	}
+
+	pthread_attr_destroy(&threadAttrs);
 	printStats();
 	if (logging != NULL)
 		stopLogging(logFile);
@@ -43,6 +122,11 @@ void runTests()
 int main(int argc, char **argv)
 {
 	parsedArgs = parseArguments(argc, argv);
+	if (parsedArgs == NULL || getTests() == FALSE)
+	{
+		testPrintf("Fatal error: There are no tests to run given on the command line!\n");
+		return 2;
+	}
 	runTests();
 	return 0;
 }
