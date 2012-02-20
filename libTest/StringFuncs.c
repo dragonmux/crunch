33f6953e529d6705b27985e664c0a4cb9521dc88
dx-mon <dx-mon@users.sourceforge.net>
2012-02-20 03:01:06 +0000

    Coppied in the formatString() function

--- libTest/StringFuncs.c
+++ libTest/StringFuncs.c
@@ -1,6 +1,9 @@
 #include "libTest.h"
 #include "StringFuncs.h"
+#include "Memory.h"
 #include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 const char *boolToString(uint8_t value)
 {
@@ -8,4 +11,19 @@ const char *boolToString(uint8_t value)
 		return "false";
 	else
 		return "true";
+}
+
+char *formatString(const char *format, ...)
+{
+	int len;
+	char *ret;
+	va_list args;
+	va_start(args, format);
+	len = vsnprintf(NULL, 0, format, args);
+	va_end(args);
+	ret = (char *)testMalloc(len + 1);
+	va_start(args, format);
+	vsprintf(ret, format, args);
+	va_end(args);
+	return ret;
 }
--- libTest/StringFuncs.h
+++ libTest/StringFuncs.h
@@ -4,5 +4,6 @@
 #include <inttypes.h>
 
 extern const char *boolToString(uint8_t value);
+extern char *formatString(const char *format, ...);
 
 #endif /* __STRINGFUNCS_H__ */
