08932bc9f7f66326d6dae1decbf2dfcb3f38dae5
dx-mon <dx-mon@users.sourceforge.net>
2014-08-24 17:31:47 +0100

    Coppied over the logger from crunch into crunch++ and adjusted it for C++ as all the concepts and code that currently comprise crunch are to be duplicated by crunch++ using C++11 utilities stuch as std::thread() whence crunch will then get rewritten in C++ using crunch++ to create the second version of the C test framework

---
+++ crunch++/Logger.cpp
@@ -0,0 +1,271 @@
+/*
+ * This file is part of crunch
+ * Copyright © 2013 Rachel Mant (dx-mon@users.sourceforge.net)
+ *
+ * crunch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * crunch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Core.h"
+#include "Logger.h"
+#ifndef _MSC_VER
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/file.h>
+#else
+#include <io.h>
+#include <sys/locking.h>
+#endif
+#include <stdarg.h>
+
+#ifndef _MSC_VER
+#define COL(val) val - 8
+#else
+#define COL(val) val - 9
+#endif
+#define WCOL(val) val - 2
+
+#ifdef _MSC_VER
+	#define TTY	"CON"
+HANDLE console;
+FILE *stdout;
+#else
+	#define TTY	"/dev/tty"
+#endif
+
+FILE *realStdout = NULL;
+uint8_t logging = 0;
+testLog *logger = NULL;
+uint8_t isTTY = 1;
+
+#ifndef _MSC_VER
+int getColumns()
+{
+	struct winsize win;
+	ioctl(STDIN_FILENO, TIOCGWINSZ, &win);
+	return (win.ws_col == 0 ? 80 : win.ws_col);
+}
+#else
+int getColumns()
+{
+	CONSOLE_SCREEN_BUFFER_INFO window;
+	GetConsoleScreenBufferInfo(console, &window);
+	return window.dwSize.X;
+}
+#endif
+
+size_t vaTestPrintf(const char *format, va_list args)
+{
+	if (realStdout == NULL)
+		realStdout = stdout;
+	return vfprintf(realStdout, format, args);
+}
+
+size_t testPrintf(const char *format, ...)
+{
+	size_t ret;
+	va_list args;
+	va_start(args, format);
+	ret = vaTestPrintf(format, args);
+	va_end(args);
+	return ret;
+}
+
+void printOk()
+{
+	testPrintf("[  OK  ]\n");
+}
+
+void printFailure()
+{
+	testPrintf("[ FAIL ]\n");
+}
+
+void printAborted()
+{
+	testPrintf("[ **** ABORTED **** ]\n");
+}
+
+#ifndef _MSC_VER
+void echoOk()
+{
+	if (isTTY != 0)
+		testPrintf(CURS_UP SET_COL BRACKET "[" SUCCESS "  OK  " BRACKET "]" NEWLINE, COL(getColumns()));
+	else
+		printOk();
+	passes++;
+}
+
+void echoFailure()
+{
+	if (isTTY != 0)
+		testPrintf(CURS_UP SET_COL BRACKET "[" FAILURE " FAIL " BRACKET "]" NEWLINE, COL(getColumns()));
+	else
+		printFailure();
+	failures++;
+}
+
+void echoAborted()
+{
+	if (isTTY != 0)
+		testPrintf(BRACKET "[" FAILURE " **** ABORTED **** " BRACKET "]" NEWLINE);
+	else
+		printAborted();
+	throw threadExit_t(0);
+}
+#else
+void echoOk()
+{
+	if (isTTY != 0)
+	{
+		CONSOLE_SCREEN_BUFFER_INFO cursor;
+		GetConsoleScreenBufferInfo(console, &cursor);
+		cursor.dwCursorPosition.Y--;
+		cursor.dwCursorPosition.X = COL(getColumns());
+		SetConsoleCursorPosition(console, cursor.dwCursorPosition);
+		SetConsoleTextAttribute(console, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+		testPrintf("[");
+		SetConsoleTextAttribute(console, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		testPrintf("  OK  ");
+		SetConsoleTextAttribute(console, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+		testPrintf("]");
+		SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		testPrintf("\n");
+	}
+	else
+		printOk();
+	passes++;
+}
+
+void echoFailure()
+{
+	if (isTTY != 0)
+	{
+		CONSOLE_SCREEN_BUFFER_INFO cursor;
+		GetConsoleScreenBufferInfo(console, &cursor);
+		cursor.dwCursorPosition.Y--;
+		cursor.dwCursorPosition.X = COL(getColumns());
+		SetConsoleCursorPosition(console, cursor.dwCursorPosition);
+		SetConsoleTextAttribute(console, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+		testPrintf("[");
+		SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_INTENSITY);
+		testPrintf(" FAIL ");
+		SetConsoleTextAttribute(console, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+		testPrintf("]");
+		SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		testPrintf("\n");
+	}
+	else
+		printFailure();
+	failures++;
+}
+
+void echoAborted()
+{
+	if (isTTY != 0)
+	{
+		CONSOLE_SCREEN_BUFFER_INFO cursor;
+		GetConsoleScreenBufferInfo(console, &cursor);
+		cursor.dwCursorPosition.Y--;
+		cursor.dwCursorPosition.X = COL(getColumns()) - 13;
+		SetConsoleCursorPosition(console, cursor.dwCursorPosition);
+		SetConsoleTextAttribute(console, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+		testPrintf("[");
+		SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_INTENSITY);
+		testPrintf(" **** ABORTED **** ");
+		SetConsoleTextAttribute(console, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+		testPrintf("]");
+		SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		testPrintf("\n");
+	}
+	else
+		printAborted();
+	libDebugExit(0);
+}
+#endif
+
+void logResult(resultType type, const char *message, ...)
+{
+	va_list args;
+
+	if (isTTY != 0)
+#ifndef _MSC_VER
+		testPrintf(NORMAL);
+#else
+		SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#endif
+	va_start(args, message);
+	vaTestPrintf(message, args);
+	va_end(args);
+	if (type != RESULT_SUCCESS && isTTY != 0)
+#ifndef _MSC_VER
+		testPrintf(NEWLINE);
+#else
+	{
+		SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		testPrintf("\n");
+	}
+#endif
+	switch (type)
+	{
+		case RESULT_SUCCESS:
+			echoOk();
+			break;
+		case RESULT_FAILURE:
+			echoFailure();
+			break;
+		default:
+			echoAborted();
+	}
+}
+
+testLog *startLogging(const char *fileName)
+{
+	testLog *ret;
+	if (logging == 1)
+		return NULL;
+	ret = new testLog;
+	logging = 1;
+#ifndef _MSC_VER
+	ret->stdout = dup(STDOUT_FILENO);
+#else
+	ret->stdout = dup(fileno(stdout));
+#endif
+	realStdout = fdopen(ret->stdout, "w");
+	ret->file = freopen(fileName, "w", stdout);
+	ret->fd = fileno(ret->file);
+#ifndef _MSC_VER
+	flock(ret->fd, LOCK_EX);
+#else
+//	locking(ret->fd, LK_LOCK, -1);
+#endif
+	logger = ret;
+	return ret;
+}
+
+void stopLogging(testLog *logFile)
+{
+	if (logFile == NULL)
+		return;
+#ifndef _MSC_VER
+	flock(logFile->fd, LOCK_UN);
+#else
+//	locking(logFile->fd, LK_UNLCK, -1);
+#endif
+	fclose(logFile->file);
+	fclose(realStdout);
+	realStdout = freopen(TTY, "w", stdout);
+	delete logFile;
+	logger = NULL;
+	logging = 0;
+}
---
+++ crunch++/Logger.h
@@ -0,0 +1,58 @@
+/*
+ * This file is part of crunch
+ * Copyright © 2013 Rachel Mant (dx-mon@users.sourceforge.net)
+ *
+ * crunch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * crunch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LOGGER_H__
+#define __LOGGER_H__
+
+#ifdef _MSC_VER
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+#include "crunch++.h"
+
+typedef enum _resultType
+{
+	RESULT_FAILURE,
+	RESULT_SUCCESS
+} resultType;
+
+CRUNCH_API uint8_t isTTY;
+#ifdef _MSC_VER
+CRUNCH_API HANDLE console;
+#endif
+CRUNCH_API uint8_t logging;
+CRUNCH_API testLog *logger;
+
+CRUNCH_API size_t vaTestPrintf(const char *format, va_list args);
+CRUNCH_API size_t testPrintf(const char *format, ...);
+CRUNCH_API void logResult(resultType type, const char *message, ...);
+
+#define COLOUR(Code) "\x1B[" Code "m"
+#define NORMAL COLOUR("0;39")
+#define SUCCESS COLOUR("1;32")
+#define FAILURE COLOUR("1;31")
+#define BRACKET COLOUR("1;34")
+#define INFO COLOUR("1;36")
+
+#define CURS_UP "\x1B[1A\x1B[0G"
+#define SET_COL "\x1B[%dG"
+
+#define NEWLINE NORMAL "\x1B[1A\x1B[s\x1B[1B\n"
+#define MOVE_END_PRINTED "\x1B[u"
+
+#endif /* __LOGGER_H__ */
