b762d71fdf68c546081687ad3b29af9d94122a16
dx-mon <dx-mon@users.sourceforge.net>
2014-08-24 17:45:59 +0100

    Moved the testing call system into it's own file as there will be more to it and organisation of crunch++ needs to be as good as in crunch if not better because it's more complex

--- crunch++/Core.cpp
+++ crunch++/Core.cpp
@@ -1,9 +1,5 @@
 #include "crunch++.h"
 #include "Logger.h"
-#include <exception>
-
-using namespace std;
-bool loggingTests = 0;
 
 testsuit::testsuit() { }
 
@@ -112,65 +108,4 @@ void testsuit::assertGreaterThan(long result, long expected)
 void testsuit::assertLessThan(long result, long expected)
 {
 	::assertLessThan(result, expected);
-}
-
-int testsuit::testRunner(cxxUnitTest &test)
-{
-	if (isTTY != 0)
-#ifndef _MSC_VER
-		testPrintf(INFO);
-#else
-		SetConsoleTextAttribute(console, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-#endif
-	testPrintf("%s...", test.theTest.testName);
-	if (isTTY != 0)
-#ifndef _MSC_VER
-		testPrintf(NEWLINE);
-#else
-		newline();
-#endif
-	else
-		testPrintf(" ");
-	try
-	{
-		cxxTest &unitTest = test.theTest;
-		(unitTest.unitClass->*unitTest.testFunc)();
-	}
-	catch (threadExit_t &val)
-	{
-		// Did the test switch logging on?
-		if (loggingTests == 0 && logging == 1)
-			// Yes, switch it back off again
-			stopLogging(logger);
-		return val;
-	}
-	catch (...)
-	{
-		exception_ptr e = current_exception();
-		// Did the test switch logging on?
-		if (loggingTests == 0 && logging == 1)
-			// Yes, switch it back off again
-			stopLogging(logger);
-		test.theTest.unitClass->fail("Exception caught by crunch++");
-	}
-	// Did the test switch logging on?
-	if (loggingTests == 0 && logging == 1)
-		// Yes, switch it back off again
-		stopLogging(logger);
-	logResult(RESULT_SUCCESS, "");
-	return 0;
-}
-
-void testsuit::test()
-{
-	for (auto &unitTest : tests)
-	{
-		cxxUnitTest test;
-		int retVal = 2;
-		test.theTest = unitTest;
-		test.testThread = thread([&]{ retVal = testRunner(test); });
-		test.testThread.join();
-		if (retVal == 2)
-			exit(2);
-	}
 }
