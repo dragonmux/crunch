ae1c37461725da68d107cb3f844e69c355e755c2
dx-mon <dx-mon@users.sourceforge.net>
2014-08-25 13:02:52 +0100

    Added crunch's StringFuncs to crunch++ directly so they can be rewritten slightly in terms of new/delete

---
+++ crunch++/StringFuncs.cpp
@@ -0,0 +1,47 @@
+/*
+ * This file is part of crunch
+ * Copyright © 2013 Rachel Mant (dx-mon@users.sourceforge.net)
+ *
+ * crunch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * crunch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "crunch.h"
+#include "StringFuncs.h"
+#include "Memory.h"
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+const char *boolToString(uint8_t value)
+{
+	if (value == FALSE)
+		return "false";
+	else
+		return "true";
+}
+
+char *formatString(const char *format, ...)
+{
+	int len;
+	char *ret;
+	va_list args;
+	va_start(args, format);
+	len = vsnprintf(NULL, 0, format, args);
+	va_end(args);
+	ret = (char *)testMalloc(len + 1);
+	va_start(args, format);
+	vsprintf(ret, format, args);
+	va_end(args);
+	return ret;
+}
