66a704f7583f5f6e1386262394f911659ae756a0
dx-mon <dx-mon@users.sourceforge.net>
2017-05-15 21:47:06 +0100

    Fixed the linkage of crunch++'s args parser

--- crunch++/ArgsParser.h
+++ crunch++/ArgsParser.h
@@ -47,12 +47,12 @@ using parsedArgs_t = std::unique_ptr<constParsedArg_t []>;
 #define ARG_REPEATABLE	1
 #define ARG_INCOMPLETE	2
 
-CRUNCH_API void registerArgs(const arg_t *allowedArgs) noexcept;
-CRUNCH_API parsedArgs_t parseArguments(const uint32_t argc, const char *const *const argv) noexcept;
-CRUNCH_API constParsedArg_t findArg(constParsedArg_t *const args, const char *const value, const constParsedArg_t defaultVal);
+CRUNCHpp_API void registerArgs(const arg_t *allowedArgs) noexcept;
+CRUNCHpp_API parsedArgs_t parseArguments(const uint32_t argc, const char *const *const argv) noexcept;
+CRUNCHpp_API constParsedArg_t findArg(constParsedArg_t *const args, const char *const value, const constParsedArg_t defaultVal);
 inline constParsedArg_t findArg(const parsedArgs_t &args, const char *const value, const constParsedArg_t defaultVal)
 	{ return findArg(args.get(), value, defaultVal); }
-CRUNCH_API const arg_t *findArgInArgs(const char *const value);
+CRUNCHpp_API const arg_t *findArgInArgs(const char *const value);
 
 bool checkAlreadyFound(const parsedArgs_t &parsedArgs, const parsedArg_t &toCheck) noexcept;
 uint32_t checkParams(const uint32_t argc, const char *const *const argv, const uint32_t argPos, const arg_t &argument, const arg_t *const args) noexcept;
