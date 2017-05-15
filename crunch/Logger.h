153484332110f7a5bf334086e6de81bae5edb59f
dx-mon <dx-mon@users.sourceforge.net>
2017-05-15 21:38:22 +0100

    Done the same cleanup of the output formatting controls for crunch's Logger as done for crunch++'s

--- crunch/Logger.h
+++ crunch/Logger.h
@@ -51,10 +51,9 @@ CRUNCH_API void logResult(resultType type, const char *message, ...);
 #define BRACKET COLOUR("1;34")
 #define INFO COLOUR("1;36")
 
-#define CURS_UP "\x1B[1A\x1B[0G"
+#define CURS_UP "\x1B[1A"
 #define SET_COL "\x1B[%dG"
 
-#define NEWLINE NORMAL "\x1B[1A\x1B[s\x1B[1B\n"
-#define MOVE_END_PRINTED "\x1B[u"
+#define NEWLINE NORMAL "\n"
 
 #endif /* __LOGGER_H__ */
