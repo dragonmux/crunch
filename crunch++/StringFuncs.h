46eee4feac05bef9c7a2b41b16c1d332d7254d16
dx-mon <dx-mon@users.sourceforge.net>
2017-11-14 01:03:30 +0000

    Fixed the string utilities for crunch++ so they don't get exposed, interfering with the coverage stats on other code that define the same symbols

--- crunch++/StringFuncs.h
+++ crunch++/StringFuncs.h
@@ -22,9 +22,9 @@
 #include "crunch++.h"
 #include "memory.hxx"
 
-CRUNCHpp_API const char *boolToString(bool value);
-CRUNCHpp_API std::unique_ptr<char []> formatString(const char *format, ...) noexcept;
-CRUNCHpp_API std::unique_ptr<char []> stringDup(const char *const str) noexcept;
-CRUNCHpp_API std::unique_ptr<const char []> strNewDup(const char *const str) noexcept;
+extern const char *boolToString(bool value);
+extern std::unique_ptr<char []> formatString(const char *format, ...) noexcept;
+extern std::unique_ptr<char []> stringDup(const char *const str) noexcept;
+extern std::unique_ptr<const char []> strNewDup(const char *const str) noexcept;
 
 #endif /* __STRINGFUNCS_H__ */
