6654df65980eda6b5cfb5e06cffe385d45985ff4
dx-mon <dx-mon@users.sourceforge.net>
2018-09-21 03:51:40 +0100

    Completed setting up a usable environment for the Meson + ninja Travis-CI build

--- .travis-ci/install.sh
+++ .travis-ci/install.sh
@@ -4,7 +4,9 @@ set -x
 
 if [ "$ENGINE" == "meson" ]; then
 	wget https://bootstrap.pypa.io/get-pip.py
+	wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
 	python3.6 get-pip.py --user
 	pip3 install --user meson
-	apt-cache show ninja-build
+	unzip ninja-linux.zip -d ~/.local/bin
+	rm get-pip.py ninja-linux.zip
 fi
