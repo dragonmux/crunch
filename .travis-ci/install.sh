7335397133ab639fccaedaa62f7d9ddad6b989a5
dx-mon <dx-mon@users.sourceforge.net>
2018-09-21 03:38:14 +0100

    More work toward a working Meson + ninja build

--- .travis-ci/install.sh
+++ .travis-ci/install.sh
@@ -5,7 +5,6 @@ set -x
 if [ "$ENGINE" == "meson" ]; then
 	wget https://bootstrap.pypa.io/get-pip.py
 	python3.6 get-pip.py --user
-	which pip3
-	apt-cache info ninja-build
 	pip3 install --user meson
+	apt-cache show ninja-build
 fi
