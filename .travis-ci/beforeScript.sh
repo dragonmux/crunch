272b985cce2c32096fbed53d8f924380ef2f695b
dx-mon <dx-mon@users.sourceforge.net>
2018-10-05 14:30:58 +0100

    Made use of the new 'exe' target for the Travis CI config

--- .travis-ci/beforeScript.sh
+++ .travis-ci/beforeScript.sh
@@ -5,9 +5,7 @@ set -x
 if [ "$ENGINE" == "make" ]; then
 	make lib
 	sudo -E make install-so
-	make -C crunch crunch
-	make -C crunch++ crunch++
-	make -C crunchMake
+	make exe
 	sudo -E make install
 elif [ "$ENGINE" == "meson" ]; then
 	CC="$CC_" CXX="$CXX_" meson build --prefix=$HOME/.local
