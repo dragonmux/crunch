b6f8d776aeafc32fb93e9f2eb512ede8b900d9e1
dx-mon <dx-mon@users.sourceforge.net>
2019-05-02 19:32:13 +0100

    Improved our meson-time handling of Windows vs Linux

--- .travis-ci/beforeScript.sh
+++ .travis-ci/beforeScript.sh
@@ -8,7 +8,13 @@ if [ "$ENGINE" == "make" ]; then
 	make exe
 	sudo -E make install
 elif [ "$ENGINE" == "meson" ]; then
-	CC="$CC_" CXX="$CXX_" meson build --prefix=$HOME/.local
+	if [ "$TRAVIS_OS_NAME" != "windows" ]; then
+		CC="$CC_" CXX="$CXX_" meson build --prefix=$HOME/.local
+	else
+		export
+		# TODO: Source/run the VS2017 vcvarsall.bat
+		meson build --prefix=$HOME/local
+	fi
 	cd build
 	ninja
 fi
