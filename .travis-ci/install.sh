932af429dc0d21fc359c088f9deab2a2d0a85625
dx-mon <dx-mon@users.sourceforge.net>
2019-05-02 02:58:33 +0100

    Switched all wgets for windows over to the "dot:giga" progress style for better output condensation and added some environment checking/debug to inspect if Python is around in case we don't actually need to install it ourselves

--- .travis-ci/install.sh
+++ .travis-ci/install.sh
@@ -3,8 +3,10 @@ unset PS4
 set -x
 
 if [ "$TRAVIS_OS_NAME" == "windows" ]; then
-	alias wget='wget --progress=dot:mega'
-	wget --progress=dot:giga https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe
+	alias wget='wget --progress=dot:giga'
+	python --version
+	python3 --version
+	wget https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe
 	wget https://bootstrap.pypa.io/get-pip.py
 	wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip
 	./python-3.6.8-amd64.exe /quiet PrependPath=1 Include_pip=0 SimpleInstall=1
