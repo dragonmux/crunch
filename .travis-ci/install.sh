1915911a9a4e11e4bda41075f83ac7f2ef76b380
dx-mon <dx-mon@users.sourceforge.net>
2019-05-02 02:20:57 +0100

    Implemented logic for setting up the front of the windows Travis CI build environment

--- .travis-ci/install.sh
+++ .travis-ci/install.sh
@@ -2,9 +2,16 @@
 unset PS4
 set -x
 
-export
-
-if [ "$ENGINE" == "meson" ]; then
+if [ "$TRAVIS_OS_NAME" == "Windows" ]; then
+	wget https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe
+	wget https://bootstrap.pypa.io/get-pip.py
+	wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip
+	./python-3.6.8-amd64.exe /quiet PrependPath=1 Include_pip=0 CompileAll=1
+	python get-pip.py --user
+	mkdir /c/tools/ninja-build
+	7z x -oC:\tools\ninja-build ninja-win.zip
+	rm python-3.6.8-amd64.exe get-pip.py ninja-win.zip
+elif [ "$ENGINE" == "meson" ]; then
 	wget https://bootstrap.pypa.io/get-pip.py
 	wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
 	python3.6 get-pip.py --user
