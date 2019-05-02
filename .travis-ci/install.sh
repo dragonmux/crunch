4780fb54f51055353eb33566ac48d1c7c90403ff
dx-mon <dx-mon@users.sourceforge.net>
2019-05-02 09:38:47 +0100

    Attempted a fix on $PATH for the windows Travis CI build and cleaned up a little in the install phase script

--- .travis-ci/install.sh
+++ .travis-ci/install.sh
@@ -6,11 +6,12 @@ if [ "$TRAVIS_OS_NAME" == "windows" ]; then
 	wget --progress=dot:mega https://bootstrap.pypa.io/get-pip.py
 	wget --progress=dot:mega https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip
 	choco install python --version 3.6.8
-	python3 get-pip.py --user
+	python --version
+	python get-pip.py --user
 	pip3 install --user meson
 	mkdir /c/tools/ninja-build
 	7z x -oC:\tools\ninja-build ninja-win.zip
-	rm python-3.6.8-amd64.exe get-pip.py ninja-win.zip
+	rm get-pip.py ninja-win.zip
 elif [ "$ENGINE" == "meson" ]; then
 	wget https://bootstrap.pypa.io/get-pip.py
 	wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
