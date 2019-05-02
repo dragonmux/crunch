87ca5a8b4f926398257a1683bd024ce440453e7d
dx-mon <dx-mon@users.sourceforge.net>
2019-05-02 18:56:13 +0100

    Fixed various path problems for the windows Travis CI build

--- .travis-ci/install.sh
+++ .travis-ci/install.sh
@@ -6,11 +6,10 @@ if [ "$TRAVIS_OS_NAME" == "windows" ]; then
 	wget --progress=dot:mega https://bootstrap.pypa.io/get-pip.py
 	wget --progress=dot:mega https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip
 	choco install python --version 3.6.8
-	python --version
-	python get-pip.py --user
-	pip3 install --user meson
+	python get-pip.py
+	pip3 install meson
 	mkdir /c/tools/ninja-build
-	7z x -oC:\tools\ninja-build ninja-win.zip
+	7z x -oC:\\tools\\ninja-build ninja-win.zip
 	rm get-pip.py ninja-win.zip
 elif [ "$ENGINE" == "meson" ]; then
 	wget https://bootstrap.pypa.io/get-pip.py
