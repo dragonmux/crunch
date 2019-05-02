a9a4966bc877e121b677ad39d7717e9cb0064e7f
dx-mon <dx-mon@users.sourceforge.net>
2019-05-02 02:52:02 +0100

    Cleanup and improvements to the windows Travis CI build install step

--- .travis-ci/install.sh
+++ .travis-ci/install.sh
@@ -4,10 +4,10 @@ set -x
 
 if [ "$TRAVIS_OS_NAME" == "windows" ]; then
 	alias wget='wget --progress=dot:mega'
-	wget https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe
+	wget --progress=dot:giga https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe
 	wget https://bootstrap.pypa.io/get-pip.py
 	wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip
-	./python-3.6.8-amd64.exe /quiet PrependPath=1 Include_pip=0 CompileAll=1
+	./python-3.6.8-amd64.exe /quiet PrependPath=1 Include_pip=0 SimpleInstall=1
 	python get-pip.py --user
 	mkdir /c/tools/ninja-build
 	7z x -oC:\tools\ninja-build ninja-win.zip
