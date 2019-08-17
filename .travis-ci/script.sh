ce334d17dc7a5c8da52f453b168e7612ed01c3e4
dx-mon <dx-mon@users.sourceforge.net>
2019-08-17 17:41:08 +0100

    Made use of 'after_failure' to dump the test logs, unifying the Meson/Ninja testing step once more for both Linux and windows Travis-CI builds

--- .travis-ci/script.sh
+++ .travis-ci/script.sh
@@ -7,10 +7,6 @@ if [ "$ENGINE" == "make" ]; then
 	make check
 elif [ "$ENGINE" == "meson" ]; then
 	cd build
-	if [ "$TRAVIS_OS_NAME" != "windows" ]; then
-		ninja test
-	else
-		meson test --no-rebuild --num-processes 1 -v
-	fi
+	ninja test
 	ninja install
 fi
