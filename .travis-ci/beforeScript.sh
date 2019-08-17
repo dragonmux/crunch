d317dacc64ec52c6ecea208f5a1e9b74ca653251
dx-mon <dx-mon@users.sourceforge.net>
2019-08-17 18:33:22 +0100

    Instructed our Travis CI scripts on how to set std_cpp to C++14 for windows only

--- .travis-ci/beforeScript.sh
+++ .travis-ci/beforeScript.sh
@@ -17,8 +17,10 @@ if [ "$ENGINE" == "make" ]; then
 	make exe
 	sudo -E make install
 elif [ "$ENGINE" == "meson" ]; then
+	EXTRA_OPTS=""
+	[ "$TRAVIS_OS_NAME" == "windows" -a "$CC_" == "clang" ] && EXTRA_OPTS="-Dcpp_std=c++14"
 	if [ "$TRAVIS_OS_NAME" != "windows" -o "$CC_" == "clang" ]; then
-		CC="$CC_" CXX="$CXX_" meson build --prefix=$HOME/.local -D b_coverage=`codecov`
+		CC="$CC_" CXX="$CXX_" meson build --prefix=$HOME/.local -D b_coverage=`codecov` $EXTRA_OPTS
 		cd build
 		ninja
 	else
--- .travis-ci/build.bat
+++ .travis-ci/build.bat
@@ -1,4 +1,4 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1
-meson build --prefix=%HOME%/local -D b_coverage=%2
+meson build --prefix=%HOME%/local -Dcpp_std=c++14 -Db_coverage=%2
 cd build
 ninja
