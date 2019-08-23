321efc361a34729ddc8d9ebd1dff9442ea8926f3
dx-mon <dx-mon@users.sourceforge.net>
2019-08-23 02:14:04 +0100

    Turned off _CRT_ERROR reporting for MSVC in an attempt at improving run stability

--- crunch/crunch.c
+++ crunch/crunch.c
@@ -270,6 +270,7 @@ int main(int argc, char **argv)
 #ifdef _WINDOWS
 	_set_invalid_parameter_handler(invalidHandler);
 	_CrtSetReportMode(_CRT_ASSERT, 0);
+	_CrtSetReportMode(_CRT_ERROR, 0);
 #endif
 	registerArgs(crunchArgs);
 	parsedArgs = parseArguments(argc, (const char **)argv);
