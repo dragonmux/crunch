ba9a007b948e644787b685b3d5eb5b4a778a0a06
dx-mon <dx-mon@users.sourceforge.net>
2020-02-23 11:54:51 +0000

    Fixed the reason crunch was dying wierdly when assertions failed

--- crunch/crunch.c
+++ crunch/crunch.c
@@ -248,7 +248,7 @@ int runTests()
 			thrd_create(&testThread, testRunner, currTest);
 			thrd_join(testThread, &retVal);
 			allocCount = -1;
-			if (retVal != THREAD_SUCCESS)
+			if (retVal == THREAD_ABORT)
 				return retVal;
 			++currTest;
 		}
