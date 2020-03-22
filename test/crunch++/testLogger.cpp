b74e5239b6cdcdc1859f7293244b3205fe9e1d73
dx-mon <dx-mon@users.sourceforge.net>
2020-03-22 16:58:57 +0000

    Sorted out several licensing and copyright headers

--- test/crunch++/testLogger.cpp
+++ test/crunch++/testLogger.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of crunch
- * Copyright © 2019 Rachel Mant (dx-mon@users.sourceforge.net)
+ * Copyright © 2019-2020 Rachel Mant (dx-mon@users.sourceforge.net)
  *
  * crunch is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
