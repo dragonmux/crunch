d32a200ed838e3283b4622c0889d3129c7e9531b
dx-mon <dx-mon@users.sourceforge.net>
2020-04-27 17:34:48 +0100

    Cleaned up in crunchMake's compilerWindows.cxx and silenced a suite of currently unfixable clang-tidy warnings

--- crunchMake/compilerWindows.cxx
+++ crunchMake/compilerWindows.cxx
@@ -5,17 +5,17 @@
 using std::literals::string_literals::operator ""s;
 
 #ifdef _DEBUG
-static const auto compileOptsExtra{"/Oi /D_DEBUG "s};
+static const auto compileOptsExtra{"/Oi /D_DEBUG "s}; // NOLINT(cert-err58-cpp)
 //" /Zi /FS"
-static const auto linkOptsExtra{"/LDd /link /DEBUG "s};
+static const auto linkOptsExtra{"/LDd /link /DEBUG "s}; // NOLINT(cert-err58-cpp)
 #else
-static const auto compileOptsExtra{"/Ox /Ob2 /Oi /Oy /GL "s};
-static const auto linkOptsExtra{"/LD /link "s};
+static const auto compileOptsExtra{"/Ox /Ob2 /Oi /Oy /GL "s}; // NOLINT(cert-err58-cpp)
+static const auto linkOptsExtra{"/LD /link "s}; // NOLINT(cert-err58-cpp)
 #endif
-static const auto compileOpts{"/Gd /GF /GS /Gy /EHsc /GT /D_WINDOWS /nologo "s};
-const std::string libExt{".tlib"s};
+static const auto compileOpts{"/Gd /GF /GS /Gy /EHsc /GT /D_WINDOWS /nologo "s}; // NOLINT(cert-err58-cpp)
+const std::string libExt{".tlib"s}; // NOLINT(cert-err58-cpp)
 
-inline const std::string crunchLib(const bool isCXX)
+inline std::string crunchLib(const bool isCXX)
 {
 	if (isCXX)
 		return "libcrunch++.lib "s;
