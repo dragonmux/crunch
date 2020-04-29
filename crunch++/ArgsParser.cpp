20cd63d2e0b9937cafd965103fa8fec371bea92c
dx-mon <dx-mon@users.sourceforge.net>
2020-04-29 11:34:19 +0100

    Namespaced the args parser and switched it to use stringView for args_t

--- crunch++/ArgsParser.cpp
+++ crunch++/ArgsParser.cpp
@@ -7,162 +7,165 @@
 #include "stringFuncs.hxx"
 #include "logger.hxx"
 
-const arg_t *args = nullptr;
-
-bool arg_t::matches(const char *const str) const noexcept
+namespace crunch
 {
-	if (flags & ARG_INCOMPLETE)
-		return !strncmp(value.data(), str, value.length());
-	return !strcmp(value.data(), str);
-}
+	static const arg_t *args = nullptr;
 
-bool parsedArg_t::matches(const char *const str) const noexcept
-	{ return matches(str, strlen(str)); }
-
-bool parsedArg_t::matches(const char *const str, const size_t length) const noexcept
-{
-	if (flags & ARG_INCOMPLETE)
-		return length >= minLength &&
-			!strncmp(value.data(), str, length);
-	return !strcmp(value.data(), str);
-}
-
-void parsedArg_t::swap(parsedArg_t &arg) noexcept
-{
-	value.swap(arg.value);
-	std::swap(paramsFound, arg.paramsFound);
-	params.swap(arg.params);
-	std::swap(flags, arg.flags);
-	std::swap(minLength, arg.minLength);
-}
-
-void registerArgs(const arg_t *allowedArgs) noexcept
-	{ args = allowedArgs; }
-
-bool checkAlreadyFound(const parsedArgs_t &parsedArgs, const parsedArg_t &toCheck) noexcept
-{
-	for (const auto &entry : parsedArgs)
+	bool arg_t::matches(const char *const str) const noexcept
 	{
-		if (entry.value == toCheck.value)
-			return true;
+		if (flags & ARG_INCOMPLETE)
+			return !strncmp(value.data(), str, value.length());
+		return !strcmp(value.data(), str);
 	}
-	return false;
-}
 
-uint32_t checkParams(const uint32_t argc, const char *const *const argv, const uint32_t argPos,
-	const arg_t &argument, const arg_t *const args_) noexcept
-{
-	uint32_t n = 0;
-	const auto min{argument.numMinParams};
-	const auto max{argument.numMaxParams};
-	bool eoa = false;
-	for (uint32_t i = argPos; i < argc && n < max && !eoa; ++i)
+	bool parsedArg_t::matches(const char *const str) const noexcept
+		{ return matches(str, strlen(str)); }
+
+	bool parsedArg_t::matches(const char *const str, const size_t length) const noexcept
 	{
-		const arg_t *currArg = args_;
-		while (!currArg->value.empty() && !eoa)
+		if (flags & ARG_INCOMPLETE)
+			return length >= minLength &&
+				!strncmp(value.data(), str, length);
+		return !strcmp(value.data(), str);
+	}
+
+	void parsedArg_t::swap(parsedArg_t &arg) noexcept
+	{
+		value.swap(arg.value);
+		std::swap(paramsFound, arg.paramsFound);
+		params.swap(arg.params);
+		std::swap(flags, arg.flags);
+		std::swap(minLength, arg.minLength);
+	}
+
+	void registerArgs(const arg_t *allowedArgs) noexcept
+		{ args = allowedArgs; }
+
+	bool checkAlreadyFound(const parsedArgs_t &parsedArgs, const parsedArg_t &toCheck) noexcept
+	{
+		for (const auto &entry : parsedArgs)
 		{
-			if (currArg->matches(argv[i]))
-				eoa = true;
-			++currArg;
+			if (entry.value == toCheck.value)
+				return true;
 		}
-		if (eoa || ++n == max)
-			break;
+		return false;
 	}
-	if (n < min)
-		return UINT32_MAX;
-	return n;
-}
 
-parsedArgs_t parseArguments(const uint32_t argc, const char *const *const argv) noexcept try
-{
-	if (argc < 1 || (argc >> 31U) == 1 || !argv || !args)
-		return {};
-
-	parsedArgs_t result{};
-	result.reserve(argc);
-	for (uint32_t i = 1; i < argc; ++i)
+	uint32_t checkParams(const uint32_t argc, const char *const *const argv, const uint32_t argPos,
+		const arg_t &argument, const arg_t *const args_) noexcept
 	{
-		bool found{};
-		bool skip{};
-		const auto *argument{args};
-		parsedArg_t entry{};
-		while (!argument->value.empty())
+		uint32_t n = 0;
+		const auto min{argument.numMinParams};
+		const auto max{argument.numMaxParams};
+		bool eoa = false;
+		for (uint32_t i = argPos; i < argc && n < max && !eoa; ++i)
 		{
-			found = argument->matches(argv[i]);
-			if (found)
+			const arg_t *currArg = args_;
+			while (!currArg->value.empty() && !eoa)
 			{
-				entry.value = argv[i];
-				entry.minLength = argument->value.length();
-				if (!(argument->flags & ARG_REPEATABLE) && checkAlreadyFound(result, entry))
+				if (currArg->matches(argv[i]))
+					eoa = true;
+				++currArg;
+			}
+			if (eoa || ++n == max)
+				break;
+		}
+		if (n < min)
+			return UINT32_MAX;
+		return n;
+	}
+
+	parsedArgs_t parseArguments(const uint32_t argc, const char *const *const argv) noexcept try
+	{
+		if (argc < 1 || (argc >> 31U) == 1 || !argv || !args)
+			return {};
+
+		parsedArgs_t result{};
+		result.reserve(argc);
+		for (uint32_t i = 1; i < argc; ++i)
+		{
+			bool found{};
+			bool skip{};
+			const auto *argument{args};
+			parsedArg_t entry{};
+			while (!argument->value.empty())
+			{
+				found = argument->matches(argv[i]);
+				if (found)
 				{
-					printf("Duplicate argument found: %s\n", entry.value.data());
-					skip = true;
+					entry.value = argv[i];
+					entry.minLength = argument->value.length();
+					if (!(argument->flags & ARG_REPEATABLE) && checkAlreadyFound(result, entry))
+					{
+						printf("Duplicate argument found: %s\n", entry.value.data());
+						skip = true;
+						break;
+					}
+					entry.paramsFound = checkParams(argc, argv, i + 1, *argument, args);
+					if (entry.paramsFound == UINT32_MAX)
+					{
+						printf("Not enough parameters given for argument %s\n", argv[i]);
+						return {};
+					}
+					// Only allocate for the params if there are any found, otherwise the list dwell as empty.
+					if (entry.paramsFound)
+					{
+						entry.params.reserve(entry.paramsFound);
+						for (uint32_t j = 0; j < entry.paramsFound; ++j)
+							entry.params.emplace_back(argv[i + j + 1]);
+					}
+					entry.params.shrink_to_fit();
+					i += entry.paramsFound;
+					entry.flags = argument->flags;
 					break;
 				}
-				entry.paramsFound = checkParams(argc, argv, i + 1, *argument, args);
-				if (entry.paramsFound == UINT32_MAX)
+				else if ((argument->flags & ARG_INCOMPLETE) &&
+					strncmp(argument->value.data(), argv[i], argument->value.length()) == 0)
 				{
-					printf("Not enough parameters given for argument %s\n", argv[i]);
+					printf("Badly formatted argument (%s)\n", argv[i]);
 					return {};
 				}
-				// Only allocate for the params if there are any found, otherwise the list dwell as empty.
-				if (entry.paramsFound)
-				{
-					entry.params.reserve(entry.paramsFound);
-					for (uint32_t j = 0; j < entry.paramsFound; ++j)
-						entry.params.emplace_back(argv[i + j + 1]);
-				}
-				entry.params.shrink_to_fit();
-				i += entry.paramsFound;
-				entry.flags = argument->flags;
-				break;
+				++argument;
 			}
-			else if ((argument->flags & ARG_INCOMPLETE) &&
-				strncmp(argument->value.data(), argv[i], argument->value.length()) == 0)
+			if (skip)
+				continue;
+			else if (!found)
 			{
-				printf("Badly formatted argument (%s)\n", argv[i]);
-				return {};
+				entry.value = argv[i];
+				entry.paramsFound = 0;
+				entry.flags = argument->flags;
+				entry.minLength = 0;
 			}
-			++argument;
+			result.emplace_back(std::move(entry));
 		}
-		if (skip)
-			continue;
-		else if (!found)
+		/* Shrink as appropriate */
+		result.shrink_to_fit();
+		return result;
+	}
+	catch (std::bad_alloc &)
+		{ return {}; }
+
+	const parsedArg_t *findArg(const parsedArgs_t &args_, const char *const value, const parsedArg_t *defaultValue)
+	{
+		if (args_.empty() || !value)
+			return defaultValue;
+		for (const auto &arg : args_)
 		{
-			entry.value = argv[i];
-			entry.paramsFound = 0;
-			entry.flags = argument->flags;
-			entry.minLength = 0;
+			if (arg.matches(value))
+				return &arg;
 		}
-		result.emplace_back(std::move(entry));
-	}
-	/* Shrink as appropriate */
-	result.shrink_to_fit();
-	return result;
-}
-catch (std::bad_alloc &)
-	{ return {}; }
-
-const parsedArg_t *findArg(const parsedArgs_t &args_, const char *const value, const parsedArg_t *defaultValue)
-{
-	if (args_.empty() || !value)
 		return defaultValue;
-	for (const auto &arg : args_)
-	{
-		if (arg.matches(value))
-			return &arg;
 	}
-	return defaultValue;
-}
 
-const arg_t *findArgInArgs(const char *const value)
-{
-	const arg_t *curr = args;
-	while (!curr->value.empty())
+	const arg_t *findArgInArgs(const char *const value)
 	{
-		if (curr->matches(value))
-			return curr;
-		++curr;
+		const arg_t *curr = args;
+		while (!curr->value.empty())
+		{
+			if (curr->matches(value))
+				return curr;
+			++curr;
+		}
+		return nullptr;
 	}
-	return nullptr;
 }
--- crunch++/argsParser.hxx
+++ crunch++/argsParser.hxx
@@ -8,57 +8,60 @@
 #include <vector>
 #include "crunch++.h"
 
-struct arg_t final
+namespace crunch
 {
-	std::string value{};
-	const uint32_t numMinParams{};
-	const uint32_t numMaxParams{};
-	const uint8_t flags{};
+	struct arg_t final
+	{
+		internal::stringView value{};
+		const uint32_t numMinParams{};
+		const uint32_t numMaxParams{};
+		const uint8_t flags{};
 
-	arg_t(std::string &&value_, const uint32_t minParams, const uint32_t maxParams, const uint8_t flags_) :
-		value{std::move(value_)}, numMinParams{minParams}, numMaxParams{maxParams}, flags{flags_} { }
-	CRUNCH_VIS bool matches(const char *const str) const noexcept;
-};
+		constexpr arg_t(const internal::stringView value_, const uint32_t minParams, const uint32_t maxParams,
+			const uint8_t flags_) : value{value_}, numMinParams{minParams}, numMaxParams{maxParams}, flags{flags_} { }
+		CRUNCH_VIS bool matches(const char *const str) const noexcept;
+	};
 
-struct parsedArg_t final
-{
-	//using strPtr_t = std::unique_ptr<const char []>;
+	struct parsedArg_t final
+	{
+		//using strPtr_t = std::unique_ptr<const char []>;
 
-	std::string value{};
-	uint32_t paramsFound{0};
-	std::vector<std::string> params{};
-	uint8_t flags{0};
-	size_t minLength{0};
+		std::string value{};
+		uint32_t paramsFound{0};
+		std::vector<std::string> params{};
+		uint8_t flags{0};
+		size_t minLength{0};
 
-	parsedArg_t() noexcept = default;
-	parsedArg_t(parsedArg_t &&arg) noexcept : parsedArg_t{} { swap(arg); }
-	void operator =(parsedArg_t &&arg) noexcept { swap(arg); }
+		parsedArg_t() noexcept = default;
+		parsedArg_t(parsedArg_t &&arg) noexcept : parsedArg_t{} { swap(arg); }
+		void operator =(parsedArg_t &&arg) noexcept { swap(arg); }
 
-	bool matches(const char *const str, const size_t length) const noexcept;
-	CRUNCH_VIS bool matches(const char *const str) const noexcept;
-	CRUNCH_VIS void swap(parsedArg_t &arg) noexcept;
+		bool matches(const char *const str, const size_t length) const noexcept;
+		CRUNCH_VIS bool matches(const char *const str) const noexcept;
+		CRUNCH_VIS void swap(parsedArg_t &arg) noexcept;
 
-	parsedArg_t(const parsedArg_t &) = delete;
-	parsedArg_t &operator =(const parsedArg_t &) = delete;
-};
+		parsedArg_t(const parsedArg_t &) = delete;
+		parsedArg_t &operator =(const parsedArg_t &) = delete;
+	};
 
-using constParsedArg_t = const parsedArg_t *;
-using parsedArgs_t = std::vector<parsedArg_t>;
-using parsedRefArgs_t = std::vector<const parsedArg_t *>;
+	using constParsedArg_t = const parsedArg_t *;
+	using parsedArgs_t = std::vector<parsedArg_t>;
+	using parsedRefArgs_t = std::vector<const parsedArg_t *>;
 
-constexpr uint8_t ARG_REPEATABLE = 1U;
-constexpr uint8_t ARG_INCOMPLETE = 2U;
+	constexpr uint8_t ARG_REPEATABLE = 1U;
+	constexpr uint8_t ARG_INCOMPLETE = 2U;
 
-CRUNCHpp_API void registerArgs(const arg_t *allowedArgs) noexcept;
-CRUNCHpp_API parsedArgs_t parseArguments(const uint32_t argc, const char *const *const argv) noexcept;
-CRUNCHpp_API const parsedArg_t *findArg(const parsedArgs_t &args, const char *const value,
-	const parsedArg_t *defaultVal);
-CRUNCHpp_API const arg_t *findArgInArgs(const char *const value);
-inline const arg_t *findArgInArgs(const std::unique_ptr<const char []> &value) { return findArgInArgs(value.get()); }
-inline const arg_t *findArgInArgs(const std::string &value) { return findArgInArgs(value.data()); }
+	CRUNCHpp_API void registerArgs(const arg_t *allowedArgs) noexcept;
+	CRUNCHpp_API parsedArgs_t parseArguments(const uint32_t argc, const char *const *const argv) noexcept;
+	CRUNCHpp_API const parsedArg_t *findArg(const parsedArgs_t &args, const char *const value,
+		const parsedArg_t *defaultVal);
+	CRUNCHpp_API const arg_t *findArgInArgs(const char *const value);
+	inline const arg_t *findArgInArgs(const std::unique_ptr<const char []> &value) { return findArgInArgs(value.get()); }
+	inline const arg_t *findArgInArgs(const std::string &value) { return findArgInArgs(value.data()); }
 
-CRUNCH_VIS bool checkAlreadyFound(const parsedArgs_t &parsedArgs, const parsedArg_t &toCheck) noexcept;
-CRUNCH_VIS uint32_t checkParams(const uint32_t argc, const char *const *const argv,
-	const uint32_t argPos, const arg_t &argument, const arg_t *const args) noexcept;
+	CRUNCHpp_API bool checkAlreadyFound(const parsedArgs_t &parsedArgs, const parsedArg_t &toCheck) noexcept;
+	CRUNCHpp_API uint32_t checkParams(const uint32_t argc, const char *const *const argv,
+		const uint32_t argPos, const arg_t &argument, const arg_t *const args) noexcept;
+}
 
 #endif /*ARGS_PARSER__H*/
