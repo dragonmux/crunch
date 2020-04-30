498a68e26116560e19b009b424e52f0157878472
dx-mon <dx-mon@users.sourceforge.net>
2020-04-30 16:44:24 +0100

    Added the machinary that was missing from cxxTestClass() as a result of having specified a non-default constructor

--- crunch++/core.hxx
+++ crunch++/core.hxx
@@ -12,11 +12,24 @@ private:
 	const char *name_;
 
 public:
+	cxxTestClass() noexcept = default;
 	cxxTestClass(std::unique_ptr<testsuite> &&suite, const char *const name) :
 		suite_{std::move(suite)}, name_{name} { }
+	cxxTestClass(const cxxTestClass &) = delete;
+	cxxTestClass(cxxTestClass &&) = default;
+	~cxxTestClass() noexcept = default;
+	cxxTestClass &operator =(const cxxTestClass &) = delete;
+	cxxTestClass &operator =(cxxTestClass &&) = default;
 
 	testsuite *suite() const noexcept { return suite_.get(); }
 	const char *name() const noexcept { return name_; }
+
+	void swap(cxxTestClass &other) noexcept
+	{
+		auto tmp{std::move(*this)};
+		*this = std::move(other);
+		other = std::move(tmp);
+	}
 };
 
 CRUNCHpp_API uint32_t passes, failures;
