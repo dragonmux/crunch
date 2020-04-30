f6bdbc4209b83034f7f9028ac3a88010b70fc950
dx-mon <dx-mon@users.sourceforge.net>
2020-04-30 16:30:39 +0100

    Better annotated the crunch and crunch++ formatString() functions for performance and compiler diagnostics

--- crunch++/stringFuncs.hxx
+++ crunch++/stringFuncs.hxx
@@ -6,8 +6,15 @@
 #include <string>
 #include "crunch++.h"
 
+#ifdef __GNUC__
+#define CRUNCH_FORMAT(style, fmtIndex, varIndex) __attribute__ ((format(style, fmtIndex, varIndex)))
+#else
+#define CRUNCH_FORMAT(style, fmtIndex, varIndex)
+#endif
+
 CRUNCHpp_API const char *boolToString(bool value);
-CRUNCHpp_API std::unique_ptr<char []> formatString(const char *format, ...) noexcept; // NOLINT
+CRUNCHpp_API std::unique_ptr<char []> formatString(const char *format, ...) noexcept // NOLINT
+	CRUNCH_FORMAT(printf, 1, 2);
 
 inline std::string operator ""_s(const char *string, const std::size_t length)
 	{ return std::string{string, length}; }
--- crunch/StringFuncs.h
+++ crunch/StringFuncs.h
@@ -5,7 +5,15 @@
 #include <stdint.h>
 #include "crunch.h"
 
+#ifdef __GNUC__
+#define CRUNCH_FORMAT(style, fmtIndex, varIndex) __attribute__ ((format(style, fmtIndex, varIndex)))
+#define CRUNCH_MALLOC __attribute__((malloc))
+#else
+#define CRUNCH_FORMAT(style, fmtIndex, varIndex)
+#define CRUNCH_MALLOC
+#endif
+
 CRUNCH_API const char *boolToString(uint8_t value);
-CRUNCH_API char *formatString(const char *format, ...);
+CRUNCH_API char *formatString(const char *format, ...) CRUNCH_FORMAT(printf, 1, 2) CRUNCH_MALLOC;
 
 #endif /*STRINGFUNCS__H*/
