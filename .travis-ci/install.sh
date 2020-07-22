85f3c74d8e72ec7264ff37a8a843fe12034185e5
dx-mon <dx-mon@users.sourceforge.net>
2020-07-22 13:14:22 +0100

    Forgot about IO redirection when writing the Meson version constraint

--- .travis-ci/install.sh
+++ .travis-ci/install.sh
@@ -18,7 +18,7 @@ else
 	if [ ! -e $HOME/.local/bin/meson ]; then
 		wget https://bootstrap.pypa.io/get-pip.py
 		python3.7 get-pip.py --user
-		pip3 install --user meson<= 0.54.3,>=0.55.1
+		pip3 install --user 'meson<= 0.54.3,>=0.55.1'
 		rm get-pip.py
 	fi
 
