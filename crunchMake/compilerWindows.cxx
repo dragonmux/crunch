ad3040d2908c593ef21c9b7d123cacde1e4d5e99
dx-mon <dx-mon@users.sourceforge.net>
2020-08-05 12:56:15 +0100

    Taught the windows version of crunchMake about being able to find crunch and crunch++'s library files for linking, and headers for compilation

--- crunchMake/compilerWindows.cxx
+++ crunchMake/compilerWindows.cxx
@@ -6,16 +6,24 @@ namespace crunch
 {
 	using namespace std::literals::string_literals;
 
+#ifdef crunch_PREFIX
+	static const auto includeOptsExtra{"/I" crunch_PREFIX "/include "s}; // NOLINT(cert-err58-cpp)
+	static const auto linkOptsExtra{"-libpath:" crunch_LIBDIR " "s}; // NOLINT(cert-err58-cpp)
+#else
+	static const auto includeOptsExtra{""s}; // NOLINT(cert-err58-cpp)
+	static const auto linkOptsExtra{""s}; // NOLINT(cert-err58-cpp)
+#endif
+
 #ifdef _DEBUG
 	static const auto compileOptsExtra{"/Oi /D_DEBUG /MDd "s}; // NOLINT(cert-err58-cpp)
 	//" /Zi /FS"
-	static const auto linkOptsExtra{"/LDd /link /DEBUG "s}; // NOLINT(cert-err58-cpp)
+	static const auto linkOpts{"/LDd /link /DEBUG "s}; // NOLINT(cert-err58-cpp)
 #else
 	static const auto compileOptsExtra{"/Ox /Ob2 /Oi /Oy /GL /MD "s}; // NOLINT(cert-err58-cpp)
-	static const auto linkOptsExtra{"/LD /link "s}; // NOLINT(cert-err58-cpp)
+	static const auto linkOpts{"/LD /link "s}; // NOLINT(cert-err58-cpp)
 #endif
 	static const auto compileOpts{"/permissive- /Zc:__cplusplus /Gd /GF /GS /Gy /EHsc /GT /D_WINDOWS /nologo "s}; // NOLINT(cert-err58-cpp)
-	const std::string libExt{".tlib"s}; // NOLINT(cert-err58-cpp)
+	const std::string libExt{".dll"s}; // NOLINT(cert-err58-cpp)
 
 	inline std::string crunchLib(const bool isCXX)
 	{
@@ -31,8 +39,8 @@ namespace crunch
 		const auto soFile{computeSOName(test)};
 		const auto objFile{computeObjName(test)};
 		const auto compileString{"cl "s + test + " "s + compileOptsExtra + compileOpts +
-			inclDirFlags + objs + "/Fe"s + soFile + " /Fo"s + objFile + " "s +
-			linkOptsExtra + libDirFlags + crunchLib(mode) + libs};
+			inclDirFlags + includeOptsExtra + objs + "/Fe"s + soFile + " /Fo"s + objFile +
+			" "s + linkOpts + linkOptsExtra + libDirFlags + crunchLib(mode) + libs};
 		if (!silent)
 		{
 			if (quiet)
