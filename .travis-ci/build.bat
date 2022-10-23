711e73c75a5e45ba9b5a36526c78626cb38a791e
L. E. Segovia <amy@amyspark.me>
2022-10-22 21:44:16 -0300

    ci: Remove Travis CI

--- .travis-ci/build.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1
-meson build --prefix=%HOME%/local -Dcpp_std=c++14 -Db_coverage=%2
-cd build
-ninja
--- .travis-ci/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-export PS4="$ "
-set -x
-
-ninja -C build test
-ninja -C build install
